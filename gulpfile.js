8d1fe9f6201b68b5886735ea9b9750b4b674d531
davecunningham <davec78@gmail.com>
2016-05-09 21:12:44 +0100

    Gulp minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css/'));
 });
+
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('css/'));
+});
+
 
 //Watch task
 gulp.task('default',function() {
