48f6e2483efa13ed43fa22b016ca2a1467fbdf4f
davecunningham <davec78@gmail.com>
2016-05-09 21:45:49 +0100

    jQuery async

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-          $(function(){
-              $(".element").typed({
-                strings: ["Digital.", "Real People.", "Love not Money."],
-                loop: true,
-                backDelay: 5000,
-                typeSpeed: 200
-              });
-          });
+  $(function(){
+      $(".element").typed({
+        strings: ["Digital.", "Real People.", "Love not Money."],
+        loop: true,
+        backDelay: 5000,
+        typeSpeed: 200
+      });
+  });
