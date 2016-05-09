d05f4bbc2b0c973bccbe6ec3297d482511140910
davecunningham <davec78@gmail.com>
2016-05-09 22:03:35 +0100

    Minify JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
+var minify = require('gulp-minify');
+
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -14,6 +16,18 @@ gulp.task('minify-css', function() {
   return gulp.src('css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('css/'));
+});
+
+
+gulp.task('compress', function() {
+  gulp.src('js/*.js')
+    .pipe(minify({
+        ext:{
+            src:'plugins.js',
+            min:'.js'
+        }
+    }))
+    .pipe(gulp.dest('js/'))
 });
 
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-  $(function(){
-      $(".element").typed({
-        strings: ["Digital.", "Real People.", "Love not Money."],
-        loop: true,
-        backDelay: 5000,
-        typeSpeed: 200
-      });
-  });
+$(function(){$(".element").typed({strings:["Digital.","Real People.","Love not Money."],loop:!0,backDelay:5e3,typeSpeed:200})});
